54af401fde5db1d29ffccdeb297d7a5b61fdc992
FeliPython <felipejpa15@gmail.com>
2024-04-21 19:07:07 -0300

    Implement routing for ArtistViewSet in Django REST API
    Updated the urls.py in Django REST to define routing for ArtistViewSet. This allows for more efficient access and management of artist data in the API. The original index view has been commented out for reference while the new router provides routing for ArtistViewSet.

--- first_api/music/urls.py
+++ first_api/music/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
+from .views import ArtistViewSet
+
+router = routers.DefaultRouter()
+router.register(r'artists', ArtistViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', include(router.urls)),
+    # path('', views.index, name='index'),
 ]

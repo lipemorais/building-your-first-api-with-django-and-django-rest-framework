3ecd5d23f809f56832daeeaeba97f6030811fc0d
FeliPython <felipejpa15@gmail.com>
2024-04-21 19:07:38 -0300

    Add ArtistViewSet in music/views.py
    Expanded the music/views.py file to include the ArtistViewSet class. Artist model data can now be efficiently accessed and managed using the Django REST API. This class handles routing for ArtistViewSet, thus improving data accessibility and manipulation.

--- first_api/music/views.py
+++ first_api/music/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from rest_framework import viewsets
+
+from music.models import Artist
+from music.serializers import ArtistSerializer
 
 
 def index(_request):
     return HttpResponse("My first API!")
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer

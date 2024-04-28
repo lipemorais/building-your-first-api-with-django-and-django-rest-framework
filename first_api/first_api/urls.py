2e1bbb6ad79fb15c811605b15c21b8f4dfca1f4c
FeliPython <felipejpa15@gmail.com>
2024-04-28 15:21:07 -0300

    Fix the index.md move to images folder accidentally

--- first_api/first_api/urls.py
+++ first_api/first_api/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('music.urls')),
+    # path('api-auth/', include('rest_framework.urls'))
 ]
